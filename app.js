36157f4a39cf0d5146bf0eb032259da91063b73d
Aaron Lo <alo76@my.bcit.ca>
2025-10-28 20:00:25 -0700

    Pipeline Manual Review change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send("Heaven or Hell, LET'S ROCK!"));
+app.get('/', (req, res) => res.send("The Wheel Of Fate Is Turning! Action!"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
